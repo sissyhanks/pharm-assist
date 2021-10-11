955aaed2d957879a95c4ef62077949af722e556c
Bernadette McKnight <sissyhanks@yahoo.com>
2021-10-11 15:35:06 -0400

    create new user route

--- server/controllers/user-controller.js
+++ server/controllers/user-controller.js
@@ -1,71 +1,39 @@
 // import user model
+const jwt = require("jsonwebtoken");
 const { User } = require('../models');
-// import sign token function from auth
-const { signToken } = require('../utils/auth');
 
 module.exports = {
-  // get a single user by either their id or their username
-  async getSingleUser({ user = null, params }, res) {
-    const foundUser = await User.findOne({
-      $or: [{ _id: user ? user._id : params.id }, { username: params.username }],
-    });
+  async register({ body }, res) {
+      const existingUser = await User.findOne({ email: body.email });
+      if (existingUser)
+        return res.status(400).json({
+          errorMessage: "There is an existing account associated with this email address."
+        });
+    
 
-    if (!foundUser) {
-      return res.status(400).json({ message: 'Cannot find a user with this id!' });
-    }
+    const newUser = await User.create(body);
 
-    res.json(foundUser);
-  },
-  // create a user, sign a token, and send it back (to client/src/components/SignUpForm.js)
-  async createUser({ body }, res) {
-    const user = await User.create(body);
-
-    if (!user) {
-      return res.status(400).json({ message: 'Something is wrong!' });
-    }
-    const token = signToken(user);
-    res.json({ token, user });
-  },
-
-  async login({ body }, res) {
-    const user = await User.findOne({ $or: [{ username: body.username }, { email: body.email }] });
-    if (!user) {
-      return res.status(400).json({ message: "Can't find this user" });
-    }
-
-    const correctPw = await user.isCorrectPassword(body.password);
-
-    if (!correctPw) {
-      return res.status(400).json({ message: 'Wrong password!' });
-    }
-    const token = signToken(user);
-    res.json({ token, user });
-  },
- 
-  async saveMed({ user, body }, res) {
-    console.log(user);
-    try {
-      const updatedUser = await User.findOneAndUpdate(
-        { _id: user._id },
-        { $addToSet: { medlist: body } },
-        { new: true, runValidators: true }
-      );
-      return res.json(updatedUser);
-    } catch (err) {
-      console.log(err);
-      return res.status(400).json(err);
-    }
-  },
-  // remove a book from `savedBooks`
-  async deleteMed({ user, params }, res) {
-    const updatedUser = await User.findOneAndUpdate(
-      { _id: user._id },
-      { $pull: { medlist: { name: params.medicineName } } },
-      { new: true }
+    const token = jwt.sign(
+      {
+        user: newUser._id,
+      },
+      "shutitupyou"
     );
-    if (!updatedUser) {
-      return res.status(404).json({ message: "Couldn't find user with this id!" });
-    }
-    return res.json(updatedUser);
+    res
+      .cookie("token", token, {
+        httpOnly: true,
+        secure: true,
+        sameSite: "none",
+      })
+      .send();
   },
-};
+}
+
+
+
+
+
+
+
+
+
--- server/routes/api/user-routes.js
+++ server/routes/api/user-routes.js
@@ -1,22 +1,29 @@
 const router = require('express').Router();
 const {
-  createUser,
-  getSingleUser,
-  saveMed,
-  deleteMed,
-  login,
+  // createUser,
+  // getSingleUser,
+  // saveMed,
+  // deleteMed,
+  // login,
+  register
 } = require('../../controllers/user-controller');
 
-// import middleware
-const { authMiddleware } = require('../../utils/auth');
+// http://localhost:3001/api/users
 
-// put authMiddleware anywhere we need to send a token for verification of user
-router.route('/').post(createUser).put(authMiddleware, saveMed);
-
-router.route('/login').post(login);
-
-router.route('/me').get(authMiddleware, getSingleUser);
-
-router.route('/medicine/:medicineName').delete(authMiddleware, deleteMed);
+router.route('/register').post(register);
 
 module.exports = router;
+
+// import middleware
+// const { authMiddleware } = require('../../utils/auth');
+
+// put authMiddleware anywhere we need to send a token for verification of user
+// router.route('/').post(createUser).put(authMiddleware, saveMed);
+
+// router.route('/login').post(login);
+
+// router.route('/me').get(authMiddleware, getSingleUser);
+
+// router.route('/medicine/:medicineName').delete(authMiddleware, deleteMed);
+
+
