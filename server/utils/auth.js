de32370e355e048032648af9e5c9c0bc1014514a
Bernadette McKnight <sissyhanks@yahoo.com>
2021-10-14 05:41:11 -0400

    final changes to server side middleware

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -5,32 +5,24 @@ const secret = 'mysecretsshhhhh';
 const expiration = '2h';
 
 module.exports = {
-  // function for our authenticated routes
   authMiddleware: function (req, res, next) {
-    // allows token to be sent via  req.query or headers
-    let token = req.query.token || req.headers.authorization;
-
-    // ["Bearer", "<tokenvalue>"]
-    if (req.headers.authorization) {
-      token = token.split(' ').pop().trim();
-    }
+    const token = req.cookies.token;
 
     if (!token) {
       return res.status(400).json({ message: 'You have no token!' });
     }
 
-    // verify token and get user data out of it
     try {
-      const { data } = jwt.verify(token, secret, { maxAge: expiration });
-      req.user = data;
-    } catch {
-      console.log('Invalid token');
-      return res.status(400).json({ message: 'invalid token!' });
+      const verified = jwt.verify(token, secret);
+      req.user = verified.user;
+    } catch (err) {
+      console.error(err);
+      res.status(401).json({ errorMessage: "Unauthorized" });
     }
 
-    // send to next endpoint
     next();
   },
+
   signToken: function ({ username, email, _id }) {
     const payload = { username, email, _id };
 
