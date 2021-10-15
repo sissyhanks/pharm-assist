8acba99c00b875af1b8115e5707bd06f0e3f934a
Bernadette McKnight <sissyhanks@yahoo.com>
2021-10-15 16:57:05 -0400

    update med list form

--- client/src/components/MedList.js
+++ client/src/components/MedList.js
@@ -12,16 +12,6 @@ export default function MedList() {
     const [ night, setNight ] = useState(false);
     const [ as_needed, setAsNeeded] = useState(false);
 
-//     const handleInputChange = (e) => {
-//     const target = e.target;
-//     const value = target.type === 'checkbox' ? target.checked : target.value;
-//     const name = target.name;
-
-//     this.setState({
-//       [name]: value
-//     });
-// }
-
     const handlenewMedSubmit = async (event) => {
         event.preventDefault();
 
@@ -38,8 +28,6 @@ export default function MedList() {
                 "http://localhost:3001/api/users/saveMed",
                 medicineData
               );
-              // await getLoggedIn();
-              // history.push("/");
             } catch (err) {
               console.error(err);
             }
@@ -60,44 +48,6 @@ export default function MedList() {
             console.error(err)
         }
     };
-        
-  
-//     const data = [{
-//       "title": "Wellbutrin",
-//       "id": 1,
-//       "morning": true,
-//       "afternoon": true,
-//       "evening": false,
-//       "night": false,
-//       "as_needed": false
-//   },
-//   {
-//       "title": "Singulair",
-//       "id": 2,
-//       "morning": false,
-//       "afternoon": false,
-//       "evening": true,
-//       "night": false,
-//       "as_needed": false
-//   },
-//   {
-//       "title": "Vitamin D",
-//       "id": 3,
-//       "morning": true,
-//       "afternoon": false,
-//       "evening": false,
-//       "night": false,
-//       "as_needed": false
-//   },
-//       {
-//       "title": "Montelukast",
-//       "id": 4,
-//       "morning": false,
-//       "afternoon": false,
-//       "evening": false,
-//       "night": true,
-//       "as_needed": false
-//   }]
 
     const [medications, setMedications] = React.useState([])
 
@@ -119,29 +69,35 @@ export default function MedList() {
 
         const renderForm = () => {
         return(
-            <div >
+            <div className="container">
+                <h3>Add a Medication to your List</h3>
             <form onSubmit={handlenewMedSubmit} className="row g-3">
+                <div className="col-md-3 form-label-inline">
+                <label htmlFor="title" className="form-label"></label>
+                <input type="text"  className="form-control" id="title" placeholder="Medication Name" onChange={(e) => setTitle(e.target.value)} value={title} />
+                </div>
+                <div className="col-md-1 form-check-inline">
+                <label htmlFor="setMorning" className="form-check-label">Morning</label>
+                <input type="checkbox"  className="form-check-input" id="setMorning" checked={morning} onChange={(e) => setMorning(e.target.checked)} value={morning} />
+                </div>
+                <div className="col-md-1 form-check-inline">
+                <label htmlFor="setAfternoon" className="form-check-label">Afternoon</label>
+                <input type="checkbox"  className="form-check-input" id="setAfternoon" checked={afternoon} onChange={(e) => setAfternoon(e.target.checked)} value={afternoon} />
+                </div>
+                <div className="col-md-1 form-check-inline">
+                <label htmlFor="setEvening" className="form-check-label">Evening</label>
+                <input type="checkbox"  className="form-check-input" id="setEvening" checked={evening} onChange={(e) => setEvening(e.target.checked)} value={evening} />
+                </div>
+                <div className="col-md-1 form-check-inline">
                 
-                <label for="title" className="form-label"></label>
-                <input type="text"  className="form-label-inline" id="title" placeholder="Medication Name" onChange={(e) => setTitle(e.target.value)} value={title} />
+                <label htmlFor="inputEmail4" className="form-check-label">Night</label>
+                <input type="checkbox"  className="" id="setNight" checked={night} onChange={(e) => setNight(e.target.checked)} value={night} />
+                </div>
+                <div className="col-md-1 form-check-inline">
                 
-                
-                <label for="setMorning" className="form-check-inline"></label>
-                <input type="checkbox"  className="form-check-inline" id="setMorning" checked={morning} onChange={(e) => setMorning(e.target.checked)} value={morning} />
-                
-                <label for="setAfternoon" className="form-check-inline"></label>
-                <input type="checkbox"  className="form-check-inline" id="setAfternoon" checked={afternoon} onChange={(e) => setAfternoon(e.target.checked)} value={afternoon} />
-                
-                <label for="setEvening" className="form-check-inline"></label>
-                <input type="checkbox"  className="form-check-inline" id="setEvening" checked={evening} onChange={(e) => setEvening(e.target.checked)} value={evening} />
-                
-                
-                <label for="inputEmail4" className="form-check-inline"></label>
-                <input type="checkbox"  className="form-check-inline" id="setNight" checked={night} onChange={(e) => setNight(e.target.checked)} value={night} />
-                
-                
-                <label for="setAsNeeded" className="form-check-inline"></label>
-                <input type="checkbox"  className="form-check-inline" id="setAsNeeded" checked={as_needed} onChange={(e) => setAsNeeded(e.target.checked)} value={as_needed} />
+                <label htmlFor="setAsNeeded" className="form-check-label">As Needed</label>
+                <input type="checkbox"  className="" id="setAsNeeded" checked={as_needed} onChange={(e) => setAsNeeded(e.target.checked)} value={as_needed} />
+                </div>
                 
                   <div className="col-12">
   <button className="btn btn-outline-info" type="submit">
@@ -154,31 +110,30 @@ export default function MedList() {
         )}
 
     const renderBody = () => {
-        return medications && medications.map(({ id, title, morning, afternoon, evening, night, as_needed }) => {
-            return (
-                <tr key={id}>
-                    <td>{id}</td>
-                    <td>{title}</td>
-                    <td><input type="checkbox" id={`medication_${id}_morning`} checked={morning} aria-label={`Checkbox for ${title} in the morning`} /></td>
-                    <td><input type="checkbox" id={`medication_${id}_afternoon`} checked={afternoon} aria-label={`Checkbox for ${title} in the afternoon`} /></td>
-                    <td><input type="checkbox" id={`medication_${id}_evening`} checked={evening} aria-label={`Checkbox for ${title} in the evening`} /></td>
-                    <td><input type="checkbox" id={`medication_${id}_night`} checked={night} aria-label={`Checkbox for ${title} in the night`} /></td>
-                    <td><input type="checkbox" id={`medication_${id}_as_needed`} checked={as_needed} aria-label={`Checkbox for ${title} as needed`} /></td>
-                </tr>
-            )
-        })
+        // return medications && medications.map(({ id, title, morning, afternoon, evening, night, as_needed }) => {
+        //     return (
+        //         <tr key={id}>
+        //             <td>{id}</td>
+        //             <td>{title}</td>
+        //             <td><input type="checkbox" id={`medication_${id}_morning`} checked={morning} aria-label={`Checkbox for ${title} in the morning`} /></td>
+        //             <td><input type="checkbox" id={`medication_${id}_afternoon`} checked={afternoon} aria-label={`Checkbox for ${title} in the afternoon`} /></td>
+        //             <td><input type="checkbox" id={`medication_${id}_evening`} checked={evening} aria-label={`Checkbox for ${title} in the evening`} /></td>
+        //             <td><input type="checkbox" id={`medication_${id}_night`} checked={night} aria-label={`Checkbox for ${title} in the night`} /></td>
+        //             <td><input type="checkbox" id={`medication_${id}_as_needed`} checked={as_needed} aria-label={`Checkbox for ${title} as needed`} /></td>
+        //         </tr>
+        //     )
+        // })
     }
 
     return (
         <>
+        <div>{renderForm()}</div>
             <h1 id='title'>Your Medication List</h1>
             <table className='table table-striped'>
                 <thead>
                     <tr>{renderHeader()}</tr>
                 </thead>
-                <div>{renderForm()}</div>
                 <tbody>
-                    
                     {renderBody()}
                 </tbody>
             </table>
