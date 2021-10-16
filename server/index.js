2f769a845a1aa00c6d2ab164160a193a0a833e19
Lance Bailey <lancebailey222@gmail.com>
2021-10-16 12:55:58 -0400

    heroku

--- server/index.js
+++ server/index.js
@@ -32,9 +32,9 @@ app.use(
 );
 
 // if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-}
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static(path.join(__dirname, '../client/build')));
+// }
 
 app.use(routes);
 
