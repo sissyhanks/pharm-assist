2c02ec134ecc6acc05d9c24314abd3466fadb1f7
Bernadette McKnight <sissyhanks@yahoo.com>
2021-10-28 16:23:38 -0400

    remove localhost

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const url = 'http://localhost:3001/api/users/' ||'https://evening-taiga-39557.herokuapp.com/';
+const url = 'https://evening-taiga-39557.herokuapp.com/';
 
 export const getSingleUser = () => axios.get(`${url}/getSingleUser`);
 export const saveMed = `${url}/saveMed`;
