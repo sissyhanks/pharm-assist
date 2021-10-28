a8379cbc6d4ff73ddf24ba9c316b9d4eca1d9908
Bernadette McKnight <sissyhanks@yahoo.com>
2021-10-28 16:42:47 -0400

    add example.env

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const url = 'http://localhost:3000/api/users' || 'https://evening-taiga-39557.herokuapp.com/api/users';
+const url = 'https://evening-taiga-39557.herokuapp.com/api/users';
 
 export const getSingleUser = () => axios.get(`${url}/getSingleUser`);
 export const saveMed = `${url}/saveMed`;
